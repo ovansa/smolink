257b675294ace67447741be8fdd0c7ec9fbd7f07
Muhammed Ibrahim <aminmuhammad18@gmail.com>
2025-04-20 08:39:09 +0100

    fix: Resolve redis url

--- pkg/database/redis.go
+++ pkg/database/redis.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/url"
+	"strings"
 	"time"
 
 	"github.com/go-redis/redis/v8"
@@ -13,13 +15,43 @@ type RedisDB struct {
 	Client *redis.Client
 }
 
-func NewRedisDB(addr, password string, db int) (*RedisDB, error) {
-	client := redis.NewClient(&redis.Options{
-		Addr:     addr,
-		Password: password,
-		DB:       db,
-	})
+// NewRedisDB creates a new Redis client. Accepts either:
+// 1. Direct connection parameters (addr, password, db)
+// 2. Redis URL format (redis://user:password@host:port/db)
+func NewRedisDB(connection string, password string, db int) (*RedisDB, error) {
+	var client *redis.Client
 
+	// If connection string is a Redis URL
+	if strings.HasPrefix(connection, "redis://") {
+		parsedURL, err := url.Parse(connection)
+		if err != nil {
+			return nil, fmt.Errorf("invalid Redis URL: %w", err)
+		}
+
+		// Extract password from URL if not explicitly provided
+		if password == "" {
+			var hasPassword bool
+			password, hasPassword = parsedURL.User.Password()
+			if !hasPassword {
+				password = "" // If no password in URL
+			}
+		}
+
+		client = redis.NewClient(&redis.Options{
+			Addr:     parsedURL.Host,
+			Password: password,
+			DB:       db, // Default to 0 if not specified in URL
+		})
+	} else {
+		// Standard connection parameters
+		client = redis.NewClient(&redis.Options{
+			Addr:     connection,
+			Password: password,
+			DB:       db,
+		})
+	}
+
+	// Verify connection
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
@@ -32,5 +64,8 @@ func NewRedisDB(addr, password string, db int) (*RedisDB, error) {
 }
 
 func (r *RedisDB) Close() error {
-	return r.Client.Close()
+	if r.Client != nil {
+		return r.Client.Close()
+	}
+	return nil
 }
